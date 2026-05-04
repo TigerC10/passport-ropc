d8402b2087bab8d42056a88a10b16dfe08f43535
Ian Cervantez <TigerC10@gmail.com>
2026-05-03 23:43:11 -0500

    feat!: updating dependencies for security

---
+++ eslint.config.mjs
@@ -0,0 +1,38 @@
+import { defineConfig, globalIgnores } from "eslint/config";
+import globals from "globals";
+import babelParser from "@babel/eslint-parser";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import js from "@eslint/js";
+import { FlatCompat } from "@eslint/eslintrc";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+    baseDirectory: __dirname,
+    recommendedConfig: js.configs.recommended,
+    allConfig: js.configs.all
+});
+
+export default defineConfig([globalIgnores(["**/node_modules/", "**/coverage/", "**/lib/"]), {
+    extends: compat.extends("prettier"),
+
+    languageOptions: {
+        globals: {
+            ...globals.browser,
+            ...globals.node,
+            ...globals.jest,
+            __DEV__: true,
+        },
+
+        parser: babelParser,
+        ecmaVersion: 6,
+        sourceType: "module",
+
+        parserOptions: {
+            ecmaFeatures: {
+                experimentalObjectRestSpread: false,
+            },
+        },
+    },
+}]);
